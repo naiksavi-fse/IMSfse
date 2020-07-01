fe4379c16ee56715ef51c7fd87e2e0b493eca047
ctsdotnetfsd06 <ctsdotnetfsd06@DOTNETFSD08>
2020-07-01 12:35:22 +0530

    Add project files.

---
+++ Controllers/EmployeeV1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_versioning_demo.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/{v:apiVersion}/employee")]
+    public class EmployeeV1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("employees from v1 controller");
+        }
+    }
+}
---
+++ Controllers/EmployeeV2Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_versioning_demo.Controllers
+{
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/{v:apiVersion}/employee")]
+    public class EmployeeV2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("employees from v2 controller");
+        }
+    }
+}
